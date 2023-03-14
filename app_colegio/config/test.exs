db9508534a011b2a85193a72b1a2f0dc0a1d4a5b
Julio Cesar <jahve20@hotmail.com>
2023-03-14 11:32:40 -0500

    Creando Proyecto CA

---
+++ app_colegio/config/test.exs
@@ -0,0 +1,15 @@
+import Config
+
+config :app_colegio, timezone: "America/Bogota"
+
+config :app_colegio,
+       http_port: 8083,
+       enable_server: true,
+       secret_name: "",
+       region: "",
+       version: "0.0.1",
+       in_test: true,
+       custom_metrics_prefix_name: "app_colegio_local"
+
+config :logger,
+       level: :debug
